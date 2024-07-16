2ea37bdce9d6907f9d2c07d98b429a74f773e272
piatachka <kislovv95@mail.ru>
2024-07-16 16:11:15 +0300

    ООП часть 2

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+class Car extends Vehicle {
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void check() {
+        System.out.println("Обслуживаем " + getModelName());
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+        checkEngine();
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
---
+++ src/ServiceStation.java
@@ -0,0 +1,5 @@
+class ServiceStation {
+    public void check(Vehicle vehicle) {
+        vehicle.check();
+    }
+}
